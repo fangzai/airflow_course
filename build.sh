6c8f7e4db7413d78390786aa33f02db44e7ce239
wanghaitao8118 <wanghaitao8118@163.com>
2024-03-17 12:25:33 +0800

    ss

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
 docker build . -f Dockerfile --tag airflow:pyspark
 docker tag airflow:pyspark wanghaitao8118/airflow:pyspark
 docker push wanghaitao8118/airflow:pyspark
+#docker rm -f $(docker ps -aq)  #删除所有的container
+#docker docker rmi image_id #删除本地image
---
+++ enter_airflow.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+docker-compose exec -u hadoop nodemaster /bin/bash
